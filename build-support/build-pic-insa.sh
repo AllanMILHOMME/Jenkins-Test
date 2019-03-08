fc950d93de8e0edda0e910ddf8a692a707e3782f
Allan MILHOMME <allan.milhomme@insa-rouen.fr>
2019-03-08 09:59:45 +0100

    Add coverage and badge script

--- build-support/build-pic-insa.sh
+++ build-support/build-pic-insa.sh
@@ -1,3 +1,7 @@
+java --jar BadgeScript.jar failed unknown
+
 git push https://$GITHUB_TOKEN@github.com/AllanMILHOMME/Jenkins-Test $CI_COMMIT_REF_NAME
 
-sbt clean coverage test coverageReport
+COVERAGE=sbt clean coverage test coverageReport | grep -E "Coverage was \[([0-9]+.[0-9]+)\%\]" | grep -Eo "([0-9]+.[0-9]+)\%"
+
+java --jar BadgeScript.jar passed $COVERAGE
