27ffc1a2ffff261e5c6bdff9d43228768abd9795
Milhomme Allan <allan.milhomme@insa-rouen.fr>
2019-03-08 09:18:00 +0100

    Add github push in build shell

--- build-support/build-pic-insa.sh
+++ build-support/build-pic-insa.sh
@@ -1,10 +1,4 @@
-TEMP_GIT_BRANCH=${GIT_BRANCH}
-MY_GIT_BRANCH="${TEMP_GIT_BRANCH#*/}"
+echo $GITHUB_TOKEN
+echo $CI_COMMIT_REF_NAME
 
-git branch tempBranch
-
-git checkout $MY_GIT_BRANCH
-git merge tempBranch
-git branch -d tempBranch
-
-git push https://$GITHUB_TOKEN@github.com/Custom2043/JenkinsTest $MY_GIT_BRANCH
+git push https://$GITHUB_TOKEN@github.com/Custom2043/JenkinsTest $CI_COMMIT_REF_NAME
