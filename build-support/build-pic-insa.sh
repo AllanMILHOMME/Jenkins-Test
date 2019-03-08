f3e3a0893eaca61fa3703c9adadc65f1484c4758
Allan MILHOMME <allan.milhomme@insa-rouen.fr>
2019-03-08 10:33:33 +0100

    Reorganize git

--- build-support/build-pic-insa.sh
+++ build-support/build-pic-insa.sh
@@ -1,9 +1,8 @@
 git checkout -b tempBranch
 git checkout $CI_COMMIT_REF_NAME
 git merge tempBranch
+git branch -d tempBranch
 
 git push https://$GITHUB_TOKEN@github.com/AllanMILHOMME/Jenkins-Test $CI_COMMIT_REF_NAME
-
-git branch -d tempBranch
 
 sbt clean coverage test coverageReport
