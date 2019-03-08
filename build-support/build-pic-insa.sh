7c267a5d648e99dd2ee4888f9f1f8a17ba8e7561
Allan MILHOMME <allan.milhomme@insa-rouen.fr>
2019-03-08 10:27:24 +0100

    Remove badges

--- build-support/build-pic-insa.sh
+++ build-support/build-pic-insa.sh
@@ -1,7 +1,3 @@
-java -jar build-support/BadgeScript.jar README.md failed ""
-
 git push https://$GITHUB_TOKEN@github.com/AllanMILHOMME/Jenkins-Test $CI_COMMIT_REF_NAME
 
-COVERAGE=$(sbt clean coverage test coverageReport | grep -E "Coverage was \[([0-9]+.[0-9]+)\]" | grep -Eo "([0-9]+.[0-9]+)")
-
-java -jar build-support/BadgeScript.jar README.md passed "$COVERAGE"
+sbt clean coverage test coverageReport
